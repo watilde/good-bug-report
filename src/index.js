aa64fdd64208a0dfc11e7ce6aa19c4b9ac39fad0
Daijiro Wachi <daijiro.wachi@gmail.com>
2016-02-12 23:51:42 +0100

    agreement link should be target blank

--- src/index.js
+++ src/index.js
@@ -65,7 +65,7 @@ $.get(endpoints.rawgit + name + '/master/CONTRIBUTING.md', function (data) {})
 module.exports = `
 <label class="mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect" for="<%= ids() %>">
   <input type="checkbox" id="<%= ids(true) %>" class="mdl-checkbox__input">
-  <span class="mdl-checkbox__label">Did you review the <a href="<%= link %>">guidelines for contributing</a> to this repository?</span>
+  <span class="mdl-checkbox__label">Did you review the <a href="<%= link %>" target="_blank">guidelines for contributing</a> to this repository?</span>
 </label>
 `
 
